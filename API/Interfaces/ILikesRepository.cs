a9fdc55dba5848f560fb021188c334568baf10ff
JulioRiambau <julioriambau@gmail.com>
2023-11-07 12:19:29 -0500

    end section 18

--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -2,12 +2,11 @@
 using API.Entities;
 using API.Helpers;
 
-namespace API.Interfaces
+namespace API.Interfaces;
+
+public interface ILikesRepository
 {
-    public interface ILikesRepository
-    {
-        Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
-        Task<AppUser> GetUserWithLikes(int userId);
-        Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams);
-    }
+    Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
+    Task<AppUser> GetUserWithLikes(int userId);
+    Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams);
 }
